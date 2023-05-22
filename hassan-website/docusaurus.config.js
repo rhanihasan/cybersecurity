29e7d579ee2713f844caff213f01243f2d642a18
rhanihasan <rhanihasan@gmail.com>
2023-05-22 21:32:35 +0530

    description changes

--- hassan-website/docusaurus.config.js
+++ hassan-website/docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Cybersecurity hasan',
-  tagline: 'Dinosaurs are cool',
+  tagline: 'Bring youor website to another level',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
