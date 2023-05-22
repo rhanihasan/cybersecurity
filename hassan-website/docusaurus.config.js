c926274de2f168a61b3126df5bbda5d766d9160c
rhanihasan <rhanihasan@gmail.com>
2023-05-22 21:44:33 +0530

    descriptions changes

--- hassan-website/docusaurus.config.js
+++ hassan-website/docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Cybersecurity hasan',
-  tagline: 'Bring youor website to another level',
+  tagline: 'Bring youor website to another level of cybersecurity',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
