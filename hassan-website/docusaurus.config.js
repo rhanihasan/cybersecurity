b57f6270cd4d5c0cf749207144d266f6cf7dd4f3
rhanihasan <rhanihasan@gmail.com>
2023-06-06 00:39:36 +0530

    improving-website-2

--- hassan-website/docusaurus.config.js
+++ hassan-website/docusaurus.config.js
@@ -81,10 +81,10 @@ const config = {
             type: 'docSidebar',
             sidebarId: 'tutorialSidebar',
             position: 'left',
-            label: 'Tutorial',
+            label: 'Cybersecurity',
           },
           
-          {to: '/blog', label: 'Blog', position: 'left'},
+          //{to: '/blog', label: 'Blog', position: 'left'},
           
           {
             href: 'https://bpdoxs.com/',
