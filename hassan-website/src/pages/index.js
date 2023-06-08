aa7a52eccd39d306225ff19bec3136f41bae2603
rhanihasan <rhanihasan@gmail.com>
2023-06-09 00:47:51 +0530

    commit3

--- hassan-website/src/pages/index.js
+++ hassan-website/src/pages/index.js
@@ -17,7 +17,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/category/cybersecurity">
+            to="/cybersecurity/docs/category/cybersecurity">
             Let Start With CyberSecurity ..
           </Link>
         </div>
